8723560f3cad12d2ff5931007fc28468acca3d66
collinsln <collangat@gmail.com>
2023-09-30 11:49:22 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+const element=document.getElementsByTagName("main");
+
+main.remove();
+function update(vic){
+
+const newHeader = document.createElement("h1");
+
+document.body.append(newHeader);
+
+let select = document.querySelector("h1")
+newHeader.id = "victory"
+newHeader.textContent = "YOUR-NAME is the champion";
+console.log(newHeader.textContent);
+
+
+}
+ update();
